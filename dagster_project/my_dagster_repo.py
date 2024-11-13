5c25ca55de6b339c4f3bdbaa8011a9be7dbb8e83
ashishdatawhiz <97599701+Ashish-Adidas@users.noreply.github.com>
2024-11-13 09:53:35 +0530

    commit

--- dagster_project/my_dagster_repo.py
+++ dagster_project/my_dagster_repo.py
@@ -1,6 +1,10 @@
 from dagster import repository
 
+<<<<<<< HEAD
 from jobs.my_job import my_job  # No change required as 'jobs' is already in 'src/'
+=======
+from jobs.my_job import my_job  # No change required as 'jobs' already in 'src/'
+>>>>>>> e4c1299ed3d03b7dd0e783b2b769d3dfc1d9383b
 
 
 @repository
